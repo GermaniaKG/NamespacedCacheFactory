6d11f430913c327a84f27b569e9bd3ecba61730a
tomkyle <412560+tomkyle@users.noreply.github.com>
2021-02-12 12:40:34 +0100

    Added docblock

--- src/PsrCacheItemPoolFactoryInterface.php
+++ src/PsrCacheItemPoolFactoryInterface.php
@@ -4,5 +4,12 @@ namespace Germania\NamespacedCache;
 
 interface PsrCacheItemPoolFactoryInterface
 {
+
+    /**
+     * Creates a namespaced PSR CacheItemPool.
+     *
+     * @param  string $namespace Namespace name
+     * @return \Psr\Cache\CacheItemPoolInterface
+     */
     public function __invoke( string $namespace) : \Psr\Cache\CacheItemPoolInterface;
 }
